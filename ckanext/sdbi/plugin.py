5c083da9cf04c2db302541c10d3597c8d21b281b
apteksdi <apteksdi@gmail.com>
2022-08-17 19:53:45 +0700

    first commit

---
+++ ckanext/sdbi/plugin.py
@@ -0,0 +1,71 @@
+from collections import OrderedDict
+import ckan.plugins as plugins
+import ckan.plugins.toolkit as toolkit
+
+
+class SDBIPlugin(plugins.SingletonPlugin):
+    plugins.implements(plugins.IConfigurer)
+    plugins.implements(plugins.IFacets, inherit=True)
+
+    # IConfigurer
+
+    def update_config(self, config_):
+        toolkit.add_template_directory(config_, 'templates')
+        toolkit.add_public_directory(config_, 'public')
+        toolkit.add_resource('fanstatic', 'sdbi')
+
+    # IFacets
+    def dataset_facets(self, facets_dict, package_type):
+
+        if package_type != 'dataset':
+            return facets_dict
+
+        return OrderedDict([('kategori', 'Kategori'),
+                            #('groups', 'Grup'),
+                            ('organization', 'Walidata'),
+                            #('vocab_category_all', 'Topic Categories'),
+                            #('metadata_type', 'Dataset Type'),
+                            #('tags', 'Tagging'),
+                            ('res_format', 'Format'),
+                            #('organization_type', 'Organization Types'),
+                            #('publisher', 'Publishers'),
+                            #('extras_progress', 'Progress'),
+                            ])
+
+    def organization_facets(self, facets_dict, organization_type, package_type):
+
+        if not package_type:
+            return OrderedDict([('organization', 'Walidata'),
+                                ('kategori', 'Kategori'),
+                                #('groups', 'Grup'),
+                                #('organization', 'Instansi'),
+                                #('vocab_category_all', 'Topic Categories'),
+                                #('metadata_type', 'Dataset Type'),
+                                #('tags', 'Tagging'),
+                                ('res_format', 'Format'),
+                                #('harvest_source_title', 'Harvest Source'),
+                                #('capacity', 'Visibility'),
+                                #('dataset_type', 'Resource Type'),
+                                #('publisher', 'Publishers'),
+                                ])
+        else:
+            return facets_dict
+
+    def group_facets(self, facets_dict, group_type, package_type):
+
+        # get the categories key
+        group_id = plugins.toolkit.c.group_dict['id']
+        key = 'vocab___category_tag_%s' % group_id
+        if not package_type:
+            return OrderedDict([('kategori', 'Kategori'),
+                                #('groups', 'Grup'),
+                                ('organization', 'Walidata'),
+                                #('metadata_type', 'Dataset Type'),
+                                #('organization_type', 'Organization Types'),
+                                #('tags', 'Tagging'),
+                                ('res_format', 'Format'),
+                                #(key, 'Categories'),
+                                #('publisher', 'Publisher'),
+                                ])
+        else:
+            return facets_dict
---
+++ ckanext/sdbi/tests/test_plugin.py
@@ -0,0 +1,5 @@
+"""Tests for plugin.py."""
+import ckanext.sdbi.plugin as plugin
+
+def test_plugin():
+    pass
